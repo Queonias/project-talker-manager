7f7ed770ecfc80b9feb695aa2a95f80390f839da
queones <queones@gmail.com>
2022-12-30 12:05:45 -0300

    fix: rota 'talker/id' configurada

--- src/routers/talker.router.js
+++ src/routers/talker.router.js
@@ -4,8 +4,22 @@ const { readTalker } = require('../utils/fsUtils');
 const router = express.Router();
 
 router.get('/talker', async (req, res) => {
-    const talkers = await readTalker();
-    res.status(200).json(talkers);
+  const talkers = await readTalker();
+  res.status(200).json(talkers);
+});
+
+router.get('/talker/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const talker = await readTalker();
+    const person = talker.find((p) => p.id === id);
+    if (person !== undefined) {
+        return res.status(200).json(person);
+    } 
+        return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  } catch (e) {
+    return res.status(500).send({ message: `Algo deu errado! Mensagem: ${e.message}` });
+  }
 });
 
 module.exports = router;
