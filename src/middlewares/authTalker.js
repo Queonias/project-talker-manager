55dcff6dc797d4a4040400aa631c6f47e9b2f641
queones <queones@gmail.com>
2022-12-30 18:53:51 -0300

    fix: criados os middlewares de autenticação da chave watchedAt

--- src/middlewares/authTalker.js
+++ src/middlewares/authTalker.js
@@ -1,6 +1,6 @@
 const validateToken = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
+  if (!authorization) { return res.status(401).json({ message: 'Token não encontrado' }); }
   if (authorization.length !== 16) {
     return res.status(401).json({ message: 'Token inválido' });
   }
@@ -9,16 +9,56 @@ const validateToken = (req, res, next) => {
 
 const validateName = (req, res, next) => {
   const { name } = req.body;
-  if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (!name) { return res.status(400).json({ message: 'O campo "name" é obrigatório' }); }
 
-  if (name.length < 3) { 
-    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  if (name.length < 3) {
+    return res
+      .status(400)
+      .json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+};
+
+const validateAge = (req, res, next) => {
+  const { age } = req.body;
+  if (!age || !Number.isInteger(age)) {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res
+      .status(400)
+      .json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+
+const validateTalk = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk) { return res.status(400).json({ message: 'O campo "talk" é obrigatório' }); }
+  next();
+};
+
+const validateWatchedAt = (req, res, next) => {
+  const isFormatDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+  const { watchedAt } = req.body.talk;
+  if (!watchedAt) {
+    return res
+        .status(400)
+        .json({ message: 'O campo "watchedAt" é obrigatório' }); 
     }
-
+  if (!isFormatDate.test(watchedAt)) {
+    return res
+      .status(400)
+      .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
   next();
 };
 
 module.exports = {
   validateToken,
   validateName,
+  validateAge,
+  validateTalk,
+  validateWatchedAt,
 };
--- src/routers/talker.router.js
+++ src/routers/talker.router.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const { readTalker, writeTalker } = require('../utils/fsUtils');
-const { validateToken, validateName } = require('../middlewares/authTalker');
+const {
+  validateToken,
+  validateName,
+  validateAge,
+  validateTalk,
+  validateWatchedAt,
+} = require('../middlewares/authTalker');
 
 const router = express.Router();
 
@@ -15,24 +21,38 @@ router.get('/talker/:id', async (req, res) => {
     const talker = await readTalker();
     const person = talker.find((p) => p.id === id);
     if (person !== undefined) {
-        return res.status(200).json(person);
-    } 
-        return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+      return res.status(200).json(person);
+    }
+    return res
+      .status(404)
+      .json({ message: 'Pessoa palestrante não encontrada' });
   } catch (e) {
-    return res.status(500).send({ message: `Algo deu errado! Mensagem: ${e.message}` });
+    return res
+      .status(500)
+      .send({ message: `Algo deu errado! Mensagem: ${e.message}` });
   }
 });
 
-router.post('/talker', validateToken, validateName, async (req, res) => {
-  try { 
-    const id = Date.now();
-    const person = req.body;
-    const personWithId = { id, ...person };
-    await writeTalker(personWithId);
-    res.status(201).json(personWithId);
-  } catch (e) {
-    res.status(500).send({ message: `Algo deu errado! Mensagem: ${e.message}` });
-  }
-});
+router.post(
+  '/talker',
+  validateToken,
+  validateName,
+  validateAge,
+  validateTalk,
+  validateWatchedAt,
+  async (req, res) => {
+    try {
+      const id = Date.now();
+      const person = req.body;
+      const personWithId = { id, ...person };
+      await writeTalker(personWithId);
+      res.status(201).json(personWithId);
+    } catch (e) {
+      res
+        .status(500)
+        .send({ message: `Algo deu errado! Mensagem: ${e.message}` });
+    }
+  },
+);
 
 module.exports = router;
