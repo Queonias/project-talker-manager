29beaa18c6f377d47594e226592fe5a554272caa
queones <queones@gmail.com>
2022-12-30 12:19:09 -0300

    fix: endpoint POST /login

--- src/routers/login.router.js
+++ src/routers/login.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const generateToken = require('../utils/generateToken');
 
 const router = express.Router();
 
 router.post('/login', (req, res) => {
-    res.status(200).json({ message: 'test' });
+    res.status(200).json({ token: generateToken() });
 });
 
 module.exports = router;
